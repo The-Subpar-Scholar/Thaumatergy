648e1cffefb7e4c1c2b8fb5ee4ea5f1b80c6b475
Oscar Price <43356609+The-Subpar-Scholar@users.noreply.github.com>
2021-11-06 19:55:31 -0700

    All bases added

--- resource/Bases.h
+++ resource/Bases.h
@@ -32,6 +32,25 @@
 #define LockTgtMan LockBonus_ptr
 #define DrawNearOnly_ptr 0x144766555
 #define debug_flags 0x144768F68
+#define NewMenuSystem 0x144776B08
+#define MenuDropData 0x14474C2E8
+#define MsgRepository 0x1447819D0
+#define WindWorld 0x14477BC00
+#define RendMan 0x1447809C8
+#define MapItemMan 0x144752300
+#define CSLoadBalancer 0x144786128
+#define CSStayParamRepository 0x144781D38
+#define CSDlc 0x144785FA0
+#define CSTrophy 0x1448880E8
+#define CSWindow 0x144889848
+#define SprjFlipper 0x144888440
+#define SprjFade 0x14477C168
+#define SprjSessionManager 0x144780990
+#define SprjHkBehManager 0x14477F2B8
+#define SprjSound 0x144784B78
+#define SprjRemo 0x14477B978
+#define SprjSfx 0x1447843C8
+
 
 //Garbage
 #define XA 0x1F90
